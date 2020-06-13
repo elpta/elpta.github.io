14306f551ca596078d12eb8808fdc128001769d3
Bo <boyang.deng@gmail.com>
2020-06-13 11:36:29 -0500

    added map descriptions

--- js/maps.js
+++ js/maps.js
@@ -12,4 +12,88 @@ function initMap() {
     var markerMixtec = new google.maps.Marker({position: mixtec, map: map});
     var markerCatalan = new google.maps.Marker({position: catalan, map: map});
     var markerAlgonquin = new google.maps.Marker({position: algonquin, map: map});
+
+    var contentStringMixtec = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<defaulth1 id="firstHeading" class="firstHeading">Mixtec</defaulth1>'+
+        '<div id="bodyContent">'+
+        '<p><b>Mixtec</b> is a large ' +
+        'sandstone rock formation in the southern part of the '+
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+        'south west of the nearest large town, Alice Springs; 450&#160;km '+
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+        'Aboriginal people of the area. It has many springs, waterholes, '+
+        'rock caves and ancient paintings. Uluru is listed as a World '+
+        'Heritage Site.</p>'+
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+        '(last visited June 22, 2009).</p>'+
+        '</div>'+
+        '</div>';
+    
+    var infowindowMixtec = new google.maps.InfoWindow({
+        content: contentStringMixtec
+    });
+    markerMixtec.addListener('click', function() {
+        infowindowMixtec.open(map, markerMixtec);
+    });
+
+    var contentStringCatalan = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<defaulth1 id="firstHeading" class="firstHeading">Catalan</defaulth1>'+
+        '<div id="bodyContent">'+
+        '<p><b>Catalan</b> is a large ' +
+        'sandstone rock formation in the southern part of the '+
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+        'south west of the nearest large town, Alice Springs; 450&#160;km '+
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+        'Aboriginal people of the area. It has many springs, waterholes, '+
+        'rock caves and ancient paintings. Uluru is listed as a World '+
+        'Heritage Site.</p>'+
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+        '(last visited June 22, 2009).</p>'+
+        '</div>'+
+        '</div>';
+    
+    var infowindowCatalan = new google.maps.InfoWindow({
+        content: contentStringCatalan
+    });
+    markerCatalan.addListener('click', function() {
+        infowindowCatalan.open(map, markerCatalan);
+    });
+
+    var contentStringAlgonquin = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<defaulth1 id="firstHeading" class="firstHeading">Algonquin</defaulth1>'+
+        '<div id="bodyContent">'+
+        '<p><b>Algonquin</b> is a large ' +
+        'sandstone rock formation in the southern part of the '+
+        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+        'south west of the nearest large town, Alice Springs; 450&#160;km '+
+        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+        'Aboriginal people of the area. It has many springs, waterholes, '+
+        'rock caves and ancient paintings. Uluru is listed as a World '+
+        'Heritage Site.</p>'+
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+        '(last visited June 22, 2009).</p>'+
+        '</div>'+
+        '</div>';
+    
+    var infowindowAlgonquin = new google.maps.InfoWindow({
+        content: contentStringAlgonquin
+    });
+    markerAlgonquin.addListener('click', function() {
+        infowindowAlgonquin.open(map, markerAlgonquin);
+    });
 }
