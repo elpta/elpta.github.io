3a686ddd95e8351a2386f35b309127e0d520e515
Rena Liang <rena_c_liang@hotmail.com>
2020-06-13 16:32:46 -0500

    Changed the descriptions on the interactive map

--- js/maps.js
+++ js/maps.js
@@ -18,19 +18,8 @@ function initMap() {
         '</div>'+
         '<defaulth1 id="firstHeading" class="firstHeading">Mixtec</defaulth1>'+
         '<div id="bodyContent">'+
-        '<p><b>Mixtec</b> is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
+        '<p><b>Mixtec</b> dialects are part of the Otomanguean languages.' +
+        'Read more about it on the <a href="about.html">about page</a>'
         '</div>'+
         '</div>';
     
@@ -46,19 +35,9 @@ function initMap() {
         '</div>'+
         '<defaulth1 id="firstHeading" class="firstHeading">Catalan</defaulth1>'+
         '<div id="bodyContent">'+
-        '<p><b>Catalan</b> is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
+        '<p><b>Catalan</b> is a Romance Language spoken in Northeastern Spain, Southern France, '+
+        'Andorra, the Balearic Islands, Alghero, and Italy. Read more about it on the '+
+        '<a href="about.html">about page</a>'+
         '</div>'+
         '</div>';
     
@@ -74,19 +53,10 @@ function initMap() {
         '</div>'+
         '<defaulth1 id="firstHeading" class="firstHeading">Algonquin</defaulth1>'+
         '<div id="bodyContent">'+
-        '<p><b>Algonquin</b> is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
+        '<p><b>Algonquin</b> belongs to the Algic Language Family ' +
+        'Based on data pulled from the 2006 and 2011 Canada Census, there were only 2,680 Algonquin speakers '+
+        'in 2006 which decreased to 1,800 Algonquin speakers in 2011'+
+        'Read more about it on the <a href="about.html">about page</a>'+
         '</div>'+
         '</div>';
     
